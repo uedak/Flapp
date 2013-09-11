83db3a4340df639ae658c49abeef46ce01a18917
uedak <webservice@aws-vm.localdomain>
2013-09-11 14:40:49 +0900

    Fix ary2tsv

--- lib/Flapp/Util/ary2tsv.pm
+++ lib/Flapp/Util/ary2tsv.pm
@@ -1,16 +1,20 @@
 use Flapp qw/-m -s -w/;
 use Encode;
 
-sub{
-    shift;
+my $a2r = sub{
     join("\t", map{
-        !defined($_)  ? '' :
-        $_ eq ''      ? '""' :
-        !/[\t\n\r"\\]/ ? $_ : do{
+        !defined($_) ? '' :
+        $_ eq ''     ? '""' : do{
             Encode::_utf8_off(my $s = $_);
-            $s =~ s/([\t\n\r"\\])/$Flapp::Util::TSV_ESC->{$1} || die $1/eg;
-            Encode::_utf8_on($s) if $Flapp::UTF8;
+            $s =~ s/([\t\n\r"\\])/$Flapp::Util::TSV_ESC->{$1} || die $1/eg if /[\t\n\r"\\]/;
             $s;
         };
-    }@_);
+    } @_)
+};
+
+sub{
+    shift;
+    return $a2r->(@_) if !$Flapp::UTF8;
+    Encode::_utf8_on(my $r = $a2r->(@_));
+    $r;
 };
