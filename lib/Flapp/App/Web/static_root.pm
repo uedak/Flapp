f8cf1b0228f94fa54e6ba2532cbede2cd265fea3
uedak <uedak@r.recruit.co.jp>
2016-11-15 02:53:47 +0900

    static_root_rev: git

--- lib/Flapp/App/Web/static_root.pm
+++ lib/Flapp/App/Web/static_root.pm
@@ -6,7 +6,13 @@ sub{
     $c->_global_->{static_root}{$prt} ||= do{
         my $cfg = $c->app_config;
         my $sr = $cfg->static_root->$prt;
-        if(my $rev = $cfg->{static_root_rev}){
+        my $rev = $cfg->{static_root_rev};
+        if($rev =~ /\bgit\z/){
+            $sr .= '_'.($c->project->_global_->{static_root_rev}{$_} ||= do{
+                my $log = $c->OS->qx('cd %path; %path log -n 1 --oneline', $_, $rev);
+                $log && $log =~ /^(\w+)\s/ ? $1 : $c->debug ? 1 : die $log
+            }) for @{$c->static_roots};
+        }elsif($rev){
             $sr .= '_'.($c->project->_global_->{static_root_rev}{$_} ||= $c->Util->i2a(
                 (stat "$_/$rev")[9] || ($c->debug ? 1 : die "$!($_/$rev)")
             )) for @{$c->static_roots};
