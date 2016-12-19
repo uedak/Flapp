61c585eeab5e0c559793f2bcf34b9347c683fdd1
uedak <uedak@r.recruit.co.jp>
2016-12-19 17:35:38 +0900

    MySQL::load_tsv change commit_per default (100 -> undef)

--- lib/Flapp/Schema/Storage/MySQL/load_tsv.pm
+++ lib/Flapp/Schema/Storage/MySQL/load_tsv.pm
@@ -14,8 +14,8 @@ sub{
         @col = @{$self->table_columns($t)};
     }
     
-    my $per = defined $opt->{commit_per} ? $opt->{commit_per} : 100;
-    my $ldr = $self->BulkLoader->new($self, $t, -c => \@col, -n => $per);
+    my $per = $opt->{commit_per};
+    my $ldr = $self->BulkLoader->new($self, $t, -c => \@col, $per ? (-n => $per) : ());
     $self->dbh->do("TRUNCATE TABLE $t") if !defined $opt->{truncate} || $opt->{truncate};
     my $cnt = 0;
     my @n = defined $opt->{strict} && !$opt->{strict} ? (0 .. $#col) : ();
